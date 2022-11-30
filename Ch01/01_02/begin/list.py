cb8ffb89a9fc560ba368d0c1e60ae48da43291d7
Craig Woodcock <craig.woodcock89@hotmail.co.uk>
2022-11-30 10:19:21 +0000

    list.py done

---
+++ Ch01/01_02/begin/list.py
@@ -0,0 +1,16 @@
+city1 = 'Tokyo'
+city2 = 'Dakar'
+city3 = 'Mumbai'
+city4 = 'Buenos Aires'
+
+#create a list with the above values
+
+cities = [
+    'Tokyo',
+    'Dakar',
+    'Mumbai',
+    'Buenos Aires',
+]
+
+print(cities[2]);
+
