396634dc284e2bbe1ecc4b8695a2e6cd68698f66
Craig Woodcock <craig.woodcock89@hotmail.co.uk>
2022-12-05 13:14:07 +0000

    whle loop complete

---
+++ Ch02/02_03/begin/while_loop.py
@@ -0,0 +1,10 @@
+print('Learnng python while loops')
+
+i = 10
+while i <=100:
+    print(i)    #prints value of i
+    i += 10     #keep incrementing i by 10 until 100 then step out of loop
+                #indentation is key!!
+
+print('end of loop')
+
