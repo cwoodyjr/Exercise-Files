c79ad31ccf42f30ab9d29ca4945bdfbb4a24c23a
Craig Woodcock <craig.woodcock89@hotmail.co.uk>
2022-12-05 13:05:26 +0000

    completed for loop

--- Ch02/02_02/begin/for_loop.py
+++ Ch02/02_02/begin/for_loop.py
@@ -4,3 +4,7 @@ spices = [
     'cumin',
     'turmeric',
 ]
+
+for spice in spices:  #creates a new variable called spice to iterate through spices list
+    print(spice)  #any indentation here will be used within the loop
+print('lovely omelette') #removing indentation places this outside the loop  
