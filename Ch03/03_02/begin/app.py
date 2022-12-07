ff36c12b49677924b9b4ff572e062485e6c1769a
Craig Woodcock <craig.woodcock89@hotmail.co.uk>
2022-12-07 17:23:06 +0000

    added dummy docstring

--- Ch03/03_02/begin/app.py
+++ Ch03/03_02/begin/app.py
@@ -1,5 +1,7 @@
+"""A dummy docstring."""
+
 import testmodule
 
-testmodule.mult(3,2)
+testmodule.mult(3, 2)
 
-testmodule.add(10,2)
+testmodule.add(10, 2)
--- Ch03/03_02/begin/testmodule.py
+++ Ch03/03_02/begin/testmodule.py
@@ -1,5 +1,11 @@
+"""A dummy docstring."""
+
+
 def mult(x, y):
+    """A function to multiply."""
     print(f'{x} * {y} = {x * y}')
 
-def add(x,y):
+
+def add(x, y):
+    """A function to add."""
     print(f'{x} + {y} = {x + y}')
