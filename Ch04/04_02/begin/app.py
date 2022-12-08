7b5e3971f371f267b12e554009a7bc4da29e3872
Craig Woodcock <craig.woodcock89@hotmail.co.uk>
2022-12-08 11:42:14 +0000

    manipulating strings

--- Ch04/04_02/begin/app.py
+++ Ch04/04_02/begin/app.py
@@ -1,3 +1,20 @@
 first_name = 'malala'
 last_name = 'yousafzai'
 note = 'award: Nobel Peace Prize'
+
+firstNameCaps = first_name.capitalize()
+lastNameCaps = last_name.capitalize()
+# note.capitalize()
+
+print(firstNameCaps)
+print(lastNameCaps)
+
+awardLocation = note.find('award:')
+print(awardLocation)
+
+awardText = note[7:]
+print(awardText)
+
+print('First Name = ' + first_name.capitalize())
+print('Last Name = ' + last_name.capitalize())
+print(note.capitalize())
