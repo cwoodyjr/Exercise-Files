3b27c9dddfe627967f02a5f74fd8015e587d1d3a
Craig Woodcock <craig.woodcock89@hotmail.co.uk>
2022-12-08 22:48:58 +0000

    regex

--- Ch04/04_03/begin/app.py
+++ Ch04/04_03/begin/app.py
@@ -3,3 +3,7 @@ import re
 five_digit_zip = '98101'
 nine_digit_zip = '98101-0003'
 phone_number = '234-567-8901'
+fiveDigitExpression = r'\d{5}'
+print(re.search(fiveDigitExpression, five_digit_zip))
+print(re.search(fiveDigitExpression, phone_number))
+print(re.search(fiveDigitExpression, nine_digit_zip))
