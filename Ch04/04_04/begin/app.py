749fa05b7c2a80d2bf95fc60d859f3dbdb64b1ab
Craig Woodcock <craig.woodcock89@hotmail.co.uk>
2022-12-08 22:59:24 +0000

    miles to km

--- Ch04/04_04/begin/app.py
+++ Ch04/04_04/begin/app.py
@@ -1,2 +1,9 @@
 miles = input('Enter a distance in miles: ')
 # kilometers_value = miles_value * 1.609344
+
+miles_float = float(miles)  # converts string into a new int variable
+
+kilometers = miles_float * 1.609334
+
+print('the value is : ')
+print(kilometers)
