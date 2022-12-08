842ab7855d2dfa874a547d9eeda8c46314825fcc
Craig Woodcock <craig.woodcock89@hotmail.co.uk>
2022-12-08 11:12:24 +0000

    concatinating strings and converting string to int

--- Ch04/04_01/begin/app.py
+++ Ch04/04_01/begin/app.py
@@ -1,2 +1,6 @@
-value = input('Enter a number: ')
-print(value)
+value = input('Enter a number: ')  # takes an input as a string
+# concatnates the string variable onto the end of a sentence
+print('My lucky number is ' + value)
+print('Multiplied by 10 gives us:')
+value_int = int(value)  # converts string into a new int variable
+print(value_int * 10)  # performs mathematical operation correctly as int variable
