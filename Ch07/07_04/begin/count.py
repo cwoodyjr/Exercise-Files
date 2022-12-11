d88a1bfde19c563847a6d56edeb649156ef594fc
Craig Woodcock <craig.woodcock89@hotmail.co.uk>
2022-12-11 15:25:28 +0000

    debugging without errors, reversing count in while loop

--- Ch07/07_04/begin/count.py
+++ Ch07/07_04/begin/count.py
@@ -1,4 +1,11 @@
 i = 10
-while i < 0:
+while i > -1:
     i -= 1
-    print(i) 
+    print(i+1)
+print('set i to 10 and print the value backwards upto and including 0')
+
+j = 0
+while j < 11:
+    j += 1
+    print((j-1))
+print('set j to 0 and print the value upto and including 10,')
